52c05b702b7c730b40c1b858b31595fe377f4e20
Nijin P S <nijingithub@gmail.com>
2022-03-19 23:34:29 +0530

    Add code to find duplicate element in array

---
+++ TIME_COMPLEXITY/src/DuplicateInArray.java
@@ -0,0 +1,23 @@
+/*
+You have been given an integer array/list(ARR) of size N which contains numbers from 0 to (N - 2).
+Each number is present at least once. That is, if N = 5, the array/list constitutes values ranging from 0 to 3,
+and among these, there is a single integer value that is present twice.
+ You need to find and return that duplicate number present in the array.
+ */
+
+import java.util.HashMap;
+
+public class DuplicateInArray {
+    public static int findDuplicate(int[] arr) {
+        //Your code goes here
+        HashMap<Integer, Integer> hashmap = new HashMap<>();
+
+        for(int val : arr){
+            if(hashmap.get(val)==null)
+                hashmap.put(val,1);
+            else
+                return val;
+        }
+        return -1;
+    }
+}
